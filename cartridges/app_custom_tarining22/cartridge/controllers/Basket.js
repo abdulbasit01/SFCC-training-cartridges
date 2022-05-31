9648f5dc6c53530d20811cbff79dfc1132f6591f
Abdul Basit <abdul.basit@royalcyber.com>
2022-05-31 15:02:54 +0500

    update code and added null check

--- cartridges/app_custom_tarining22/cartridge/controllers/Basket.js
+++ cartridges/app_custom_tarining22/cartridge/controllers/Basket.js
@@ -3,17 +3,22 @@
  * @namespace Basket
  */
 var server = require('server');
+var URLUtils = require('dw/web/URLUtils');
 server.get("Show", function (req, res, next) {
     var BasketMgr = require('dw/order/BasketMgr');
     var currentBasket = BasketMgr.getCurrentBasket();
-    var allLineItems = currentBasket.allProductLineItems;
     // res.render('basket', {basket: parseInt(currentBasket.productQuantityTotal) || "no"});
-    res.render('basket',
-        {
-            allLineItems: allLineItems || "no",
-            allLineItemsLength: allLineItems.length
-        }
-    );
+    if (currentBasket.allProductLineItems.length) {
+        res.render('basket',
+            {
+                allLineItems: currentBasket.allProductLineItems || "no",
+                allLineItemsLength: currentBasket.allProductLineItems.length
+            }
+        );
+
+    } else {
+        res.redirect(URLUtils.url('Home-Show'));
+    }
     next();
 });
 module.exports = server.exports();
