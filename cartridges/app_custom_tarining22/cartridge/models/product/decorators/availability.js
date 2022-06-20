399f7da9ac7e2799035fa2206f256a9cbe136bf7
Abdul Basit <abdul.basit@royalcyber.com>
2022-06-20 19:37:31 +0500

    form added and bug in override of css and js

--- cartridges/app_custom_tarining22/cartridge/models/product/decorators/availability.js
+++ cartridges/app_custom_tarining22/cartridge/models/product/decorators/availability.js
@@ -1,17 +1,6 @@
 'use strict';
-
 var Resource = require('dw/web/Resource');
 var base = module.superModule;
-module.exports = function (object, quantity, minOrderQuantity, availabilityModel) {
-    // Invoke the availability model on the base
-    base.call(this, object, quantity, minOrderQuantity, availabilityModel);
-
-    // Define a new property in the model with ATS as its value
-    Object.defineProperty(object, 'ats', {
-        enumerable: true,
-        value: getATSMessage(availabilityModel)
-    });
-};
 function getATSMessage(availabilityModel) {
     var ATS = {};
     ATS.messages = [];
@@ -31,3 +20,14 @@ function getATSMessage(availabilityModel) {
 
     return ATS;
 }
+
+module.exports = function (object, quantity, minOrderQuantity, availabilityModel) {
+    // Invoke the availability model on the base
+    base.call(this, object, quantity, minOrderQuantity, availabilityModel);
+
+    // Define a new property in the model with ATS as its value
+    Object.defineProperty(object, 'ats', {
+        enumerable: true,
+        value: getATSMessage(availabilityModel)
+    });
+};
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = [{
     name: 'js',
     entry: jsFiles,
     output: {
-        path: path.resolve('./cartridges/app_custom_tarining22/cartridge/static'),
+        path: path.resolve('./cartridges/app_storefront_base/cartridge/static'),
         filename: '[name].js'
     },
     module: {
@@ -49,7 +49,7 @@ module.exports = [{
     name: 'scss',
     entry: scssFiles,
     output: {
-        path: path.resolve('./cartridges/app_custom_tarining22/cartridge/static'),
+        path: path.resolve('./cartridges/app_storefront_base/cartridge/static'),
         filename: '[name].css'
     },
     module: {
@@ -85,3 +85,4 @@ module.exports = [{
         new ExtractTextPlugin({ filename: '[name].css' })
     ]
 }];
+
